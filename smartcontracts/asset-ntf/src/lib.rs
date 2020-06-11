2dec812fc1534337694614440fb6e24788944e86
damiradrovic1116 <damir@wiv.io>
2020-06-12 01:02:13 +0200

    WIP assethandler token methods

--- smartcontracts/asset-ntf/src/lib.rs
+++ smartcontracts/asset-ntf/src/lib.rs
@@ -164,18 +164,104 @@ contract! {
 
     /// Private Methods
     impl NFToken {
+
         fn is_token_owner(&self, of: &AccountId, token_id: u64) -> bool {
+            let owner = self.id_to_owner.get(&token_id);
+            if let None = owner {
+                return false;
+            }
+            let owner = *owner.unwrap();
+            if owner != *of {
+                return false;
+            }
             true
         }
 
         /// Transfers token from a specified address to another address.
         fn transfer_impl(&mut self, from: AccountId, to: AccountId, token_id: u64) -> bool {
+            if !self.is_token_owner(&from, token_id) {
+                return false;
+            }
+
+            self.id_to_owner.insert(token_id, to);
+
+            //update owner token counts
+            let from_owner_count = *self.owner_to_token_count.get(&from).unwrap_or(&0);
+            let to_owner_count = *self.owner_to_token_count.get(&to).unwrap_or(&0);
+
+            self.owner_to_token_count.insert(from, from_owner_count - 1);
+            self.owner_to_token_count.insert(to, to_owner_count + 1);
             true
         }
 
         /// minting of new tokens implementation
         fn mint_impl(&mut self, receiver: AccountId, value: u64) -> bool {
+
+            let start_id = *self.total_minted + 1;
+            let stop_id = *self.total_minted + value;
+
+            // loop through new tokens being minted
+            for token_id in start_id..stop_id {
+                self.id_to_owner.insert(token_id, receiver);
+            }
+
+            // update total supply of owner
+            let from_owner_count = *self.owner_to_token_count.get(&self.owner).unwrap_or(&0);
+            self.owner_to_token_count.insert(*self.owner, from_owner_count + value);
+
+            // update total supply
+            self.total_minted += value;
             true
         }
+    }
+}
+
+#[cfg(all(test, feature = "test-env"))]
+mod tests {
+    use super::*;
+    use std::convert::TryFrom;
+
+    #[test]
+    fn it_works() {
+
+        // deploying and minting initial tokens
+        let mut _nftoken = NFToken::deploy_mock(100);
+        let alice = AccountId::try_from([0x0; 32]).unwrap();
+        let bob = AccountId::try_from([0x1; 32]).unwrap();
+        let charlie = AccountId::try_from([0x2; 32]).unwrap();
+        let dave = AccountId::try_from([0x3; 32]).unwrap();
+
+        let total_minted = _nftoken.total_minted();
+        assert_eq!(total_minted, 100);
+
+        // transferring token_id from alice to bob
+        _nftoken.transfer(bob, 1);
+
+        let alice_balance = _nftoken.balance_of(alice);
+        let mut bob_balance = _nftoken.balance_of(bob);
+
+        assert_eq!(alice_balance, 99);
+        assert_eq!(bob_balance, 1);
+
+        // approve charlie to send token_id 2 from alice's account
+        _nftoken.approval(charlie, 2, true);
+        assert_eq!(_nftoken.is_approved(2, charlie), true);
+
+        // overwrite charlie's approval with dave's approval
+        _nftoken.approval(dave, 2, true);
+        assert_eq!(_nftoken.is_approved(2, dave), true);
+
+        // remove dave from approvals
+        _nftoken.approval(dave, 2, false);
+        assert_eq!(_nftoken.is_approved(2, dave), false);
+
+        // transfer_from function: caller is token owner
+        _nftoken.approval(charlie, 3, true);
+        assert_eq!(_nftoken.is_approved(3, charlie), true);
+
+        _nftoken.transfer_from(bob, 3);
+        bob_balance = _nftoken.balance_of(bob);
+
+        assert_eq!(bob_balance, 2);
     }
 }
